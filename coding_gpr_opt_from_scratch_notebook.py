fixed stochastic gradient descent to work with learning rate | added legends to bayes opt

coding_gpr_opt_from_scratch_notebook.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process.kernels import Matern
@@ -20,8 +19,7 @@ def plot_GPRA(ax, data_x, data_y, model, x, kappa=2,visible=True) -> list:
 
     std = np.sqrt(model._memory['variance'])
     ucb = mean + kappa * std
-    print(x[np.argmax(ucb)], np.max(ucb))
-    plt.scatter(x[np.argmax(ucb)], np.max(ucb), marker='*', c ='red', label="UCB Max")
+    plt.scatter(x[np.argmax(ucb)], np.max(ucb), marker='*', c ='red', label=f"UCB Max: {x[np.argmax(ucb)]:0.1f}")
 
     #plt.plot(data_x, data_y)
     for i in range(1, kappa+1):
@@ -31,6 +29,7 @@ def plot_GPRA(ax, data_x, data_y, model, x, kappa=2,visible=True) -> list:
 
     ax.set_xlim(-2, 10)
     ax.set_ylim(0, 2) 
+    plt.legend(loc = 'upper right')
 
     return x[np.argmax(ucb)]
 

emis_stochastic_grad_descent.py
@@ -16,19 +16,25 @@ stochastic_jump = 10
 
 x = np.random.randint(-2,10)
 
+mrk = 'x' 
+color = 'red'
 for i in range(1000):
-    mrk = 'o'
-    color = 'red'
-    if i %10 == 0 and i != 0:
+
+    x_new = x + alpha*dfdw(x)
+    if ((x_new-x)**2) < 0.1e-6:
         mrk = 'x' 
         color = 'blue'
-        x = np.random.randint(-2,10)
+        x_new = np.random.randint(-2,10)
 
-    x = x + alpha*dfdw(x)
-    plt.scatter(x, f(x), color=color, marker = mrk)
+    plt.scatter(x_new, f(x_new), color=color, marker = mrk)
     loss = ((x-xmax)**2)/2
     if loss < 0.1e-6:
         print("Loss:", loss, "Iterations:", i)
         break
+
+
+    mrk = 'o'
+    color = 'orange'
+    x = x_new
 
 plt.show()