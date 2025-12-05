c260b9ad447a2f07ae0e4ad1bd1797b666d1a83d
Emiliano Islas <eislasq@dhcp-10-142-143-244.uawifi.arizona.edu>
2025-12-05 15:43:54 -0700

    fixed euler integrator

--- scratch_integrator.py
+++ scratch_integrator.py
@@ -37,17 +37,29 @@ def rk4Integrate(interp_fun, y0, dt=0.1, length=0):
 
     return t, y
 
+def eulerIntegrate(interp_fun, y0, dt=0.1, length=0):
+    d = int(np.shape(y0)[0])
+    n_steps = int(length/dt)
+    t = np.arange(0, length+dt, dt)
+    y = np.zeros((n_steps+1, d))
+    y[0] = y0
+
+    for i in range(n_steps):
+        dydt = interp_fun(y[i])      
+        y[i+1] = y[i] + dt * dydt 
+
+    return t, y
+
 y0 = [1, 0]
 
-t, y_hat = rk4Integrate(ode_fun, y0, length = 10)
+t, y_hat_rk = rk4Integrate(ode_fun, y0, length = 10)
+t, y_hat_eul = eulerIntegrate(ode_fun, y0, length = 10)
 x_analytical, xdot_analytical = ode_analytical_sol(t)
 
 plt.plot(t, x_analytical, color = "k", label = r"$x$")
 
-plt.plot(t,xdot_analytical, color = "k", label = r"$\dot{x}$")
-
-plt.plot(t, y_hat[:,0], color = "blue", linestyle = ":", label = r"$\hat{x}$")
-plt.plot(t, y_hat[:,1], color = "red", linestyle = ":", label = r"$\hat{\dot{x}}$")
+plt.plot(t, y_hat_rk[:,0], color = "blue", linestyle = "--", label = r"rk4: $\hat{x}$")
+plt.plot(t, y_hat_eul[:,0], color = "green", linestyle = ":", label = r"euler: $\hat{x}$")
 plt.legend()
 plt.show()
 
